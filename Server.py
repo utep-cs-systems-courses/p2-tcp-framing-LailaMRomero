bdcc3cc757c708683cc069890e7c96fa6068a53b
LailaMRomero <lmromero4@miners.utep.edu>
2021-03-07 20:29:49 -0700

    new file

---
+++ Server.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+
+import re, os, sys
+import socket
+sys.path.append("../lib")# for params
+import params
+from FTPSock import *
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "fileServer"
+paramMap = params.parseParams(switchesVarDefaults)
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+    params.usage()
+
+lSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+bindAddr = ("127.0.0.1", listenPort)
+lSock.bind(bindAddr)
+
+lSock.listen(1) #listen for a connection
+myPrint('Listening in ' + str(bindAddr))
+
+while 1:
+    conn, addr = lSock.accept() #accept the connection
+    myPrint('Connected to: ' + str(addr))
+    ftp_send_Hello(conn)
+
+    try:
+        fCont = ftp_recv(conn)
+        file_name = fCont[:fCont.index('NAME')] #attain the filename
+        myWrite(file_name, fCont[fCont.index('NAME') + 2:])
+        myPrint('Completed file transfer the transfer') #Complete message
+        break
+    except:
+        myPrint('Error: File Transfer not accomplished')
+        sys.exit(1)
+        break
+conn.close() #formally close the socket
